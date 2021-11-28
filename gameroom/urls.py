449ca79bd61deb71f5ca57277a5b100e75c78e62
Ario124 <47329632+Ario124@users.noreply.github.com>
2021-11-28 14:37:09 +0100

    Added home.html to test setup

--- gameroom/urls.py
+++ gameroom/urls.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.http import HttpResponse
 
-def home(request):
-    return HttpResponse('Hello World!')
+# def home(request):
+#     return HttpResponse('Hello World!')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home),
+    path('', include('base.urls')),
 ]
