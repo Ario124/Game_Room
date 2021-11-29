18746aa55dafe522d6e5a62ddcdd22e06e019635
Ario124 <47329632+Ario124@users.noreply.github.com>
2021-11-29 21:11:38 +0100

    Added login/logout page and custom Model form to log in using Email

--- base/views.py
+++ base/views.py
@@ -1,4 +1,10 @@
+from django.db.models import expressions
+from django.contrib import messages
 from django.shortcuts import render, redirect
+# from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
+
+from .models import User
 
 # Create your views here.
 
@@ -8,3 +14,32 @@ def home(request):
 def room(request):
     return render(request, 'base/room.html')
 
+def loginPage(request):
+    page = 'login'
+
+    if request.user.is_authenticated:
+        return redirect('room')
+
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        try:
+            user = User.objects.get(email=email)
+        except:
+            messages.error(request, 'User does not exist')
+
+        user = authenticate(request, email=email, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('room')
+        else:
+            messages.error(request, 'Username OR password does not exist')
+
+    context = {'page': page}
+    return render(request, 'base/login.html', context)
+
+def logoutPage(request):
+    logout(request)
+    return redirect('home')
