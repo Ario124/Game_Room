d4f7640e237a1ff1cf724f686802e7cc2390e2e9
Ario124 <47329632+Ario124@users.noreply.github.com>
2021-12-04 20:40:45 +0100

    Added Edit & Delete functionality

--- base/urls.py
+++ base/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     path('logout/', views.logoutPage, name="logout"),
 
     path('create-room/', views.createRoom, name="create-room"),
+    path('update-room/<str:pk>/', views.updateRoom, name="update-room"),
+    path('delete-room/<str:pk>/', views.deleteRoom, name="delete-room"),
 ]
