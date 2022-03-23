60b4b8d3b2fd1bf60b1cb93533cd8806543ec1cf
Ario124 <47329632+Ario124@users.noreply.github.com>
2022-03-23 18:22:09 +0100

    Updated settings.py to change database connection

--- gameroom/settings.py
+++ gameroom/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 import environ
 import django_heroku
 import dj_database_url
@@ -88,12 +89,15 @@ WSGI_APPLICATION = 'gameroom.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if "DATABASE_URL" in os.environ:
+    DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
 
 
 # Password validation
