d8328e9eaad4c82272470c085e3e580d3b95f79f
Ario124 <47329632+Ario124@users.noreply.github.com>
2022-03-24 18:01:38 +0100

    Commented settings to be used when using Heroku

--- gameroom/settings.py
+++ gameroom/settings.py
@@ -32,9 +32,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
 
-ALLOWED_HOSTS = ['game-room-ht.herokuapp.com']
+#### USER FOR HEROKU ###
+
+# DEBUG = False
+
+# ALLOWED_HOSTS = ['game-room-ht.herokuapp.com']
+
+DEBUG = True
+
+ALLOWED_HOSTS = ['']
 
 
 # Application definition
@@ -89,15 +96,27 @@ WSGI_APPLICATION = 'gameroom.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-if "DATABASE_URL" in os.environ:
-    DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))}
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
+
+#### USER FOR HEROKU ###
+
+# if "DATABASE_URL" in os.environ:
+#     DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))}
+# else:
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'NAME': BASE_DIR / 'db.sqlite3',
+#         }
+#     }
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
+}
+
 
 
 # Password validation
