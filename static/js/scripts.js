11fcf325291b194127b7d4a8a665a50dd108d5e2
Ario124 <47329632+Ario124@users.noreply.github.com>
2022-04-14 13:46:22 +0200

    Added swipe touch for mobile to get the sidenavigation.

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,7 +1,9 @@
 window.addEventListener('DOMContentLoaded', event => {
-
     // Toggle the side navigation
     const sidebarToggle = document.body.querySelector('#sidebarToggle');
+    const sidebarTogglez = document.body.querySelector('#box');
+    let element = document.getElementById('sidebarToggle');
+    let box = document.getElementById('box');
     if (sidebarToggle) {
         sidebarToggle.addEventListener('click', event => {
             event.preventDefault();
@@ -10,4 +12,57 @@ window.addEventListener('DOMContentLoaded', event => {
         });
     }
 
+
+
 });
+
+
+// Touch the side navigation
+let touchstartY = 0;
+let touchendY = 0;
+let touchstartX = 0
+let touchendX = 0
+
+const gestureZone = document.getElementById('box');
+
+gestureZone.addEventListener('touchstart', function(event) {
+    touchstartX = event.changedTouches[0].screenX;
+    touchstartY = event.changedTouches[0].screenY;
+}, false);
+
+gestureZone.addEventListener('touchend', function(event) {
+    touchendX = event.changedTouches[0].screenX;
+    touchendY = event.changedTouches[0].screenY;
+    handleGesture();
+}, false); 
+
+function handleGesture() {
+    
+    // Swipe left
+
+    if (touchendX <= touchstartX) {
+        document.body.classList.remove('sb-sidenav-toggled');
+    }
+    
+    // Swipe right
+    
+    if (touchendX >= touchstartX) {
+        document.body.classList.add('sb-sidenav-toggled');
+    }
+    
+
+    
+    // Swipe directions
+
+    if (touchendY <= touchstartY) {
+        // console.log('Swiped up');
+    }
+    
+    if (touchendY >= touchstartY) {
+        // console.log('Swiped down');
+    }
+    
+    if (touchendY === touchstartY) {
+        // console.log('Tap');
+    }
+}
